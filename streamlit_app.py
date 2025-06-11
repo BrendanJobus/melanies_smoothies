00f8ac06e8925a3b0da2a796171688c72f0c24b5
Brendan Jobus <brendanjobus@gmail.com>
2025-06-11 10:23:58 +0100

    Update streamlit_app.py
    troubleshooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 # Convert the Snowpard Dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
