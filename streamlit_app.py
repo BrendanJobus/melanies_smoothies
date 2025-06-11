bbadc44fbde9f1d138de5cb62f27ba1894f9d77a
Brendan Jobus <brendanjobus@gmail.com>
2025-06-11 10:20:08 +0100

    Update streamlit_app.py
    Readding in the FRUIT_NAME column

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your Smoothie will be:', name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 
 # Convert the Snowpard Dataframe to a Pandas Dataframe so we can use the LOC function
@@ -38,8 +38,8 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
-        st.stop()
+        #st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
